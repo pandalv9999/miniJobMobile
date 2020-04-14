612181354328031776194fa0494e130706c56331
Rujun Yao <rujunyao@Rujuns-MacBook-Pro-2.local>
2020-04-13 23:02:27 -0700

    Set up login and register fragment layout

---
+++ app/src/main/java/com/example/minijobmobile/onboarding/OnBoardingActivity.java
@@ -0,0 +1,16 @@
+package com.example.minijobmobile.onboarding;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.minijobmobile.R;
+
+public class OnBoardingActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_on_boarding);
+    }
+}
