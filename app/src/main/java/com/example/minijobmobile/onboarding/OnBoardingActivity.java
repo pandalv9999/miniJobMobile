01f584ab6b061fa934071250c22464a915d851e6
Rujun Yao <rujunyao@Rujuns-MacBook-Pro-2.local>
2020-04-13 23:25:29 -0700

    OnBoardingActivity layout completed

--- app/src/main/java/com/example/minijobmobile/onboarding/OnBoardingActivity.java
+++ app/src/main/java/com/example/minijobmobile/onboarding/OnBoardingActivity.java
@@ -1,16 +1,53 @@
 package com.example.minijobmobile.onboarding;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentPagerAdapter;
+import androidx.viewpager.widget.ViewPager;
 
 import android.os.Bundle;
 
 import com.example.minijobmobile.R;
 
+import java.util.ArrayList;
+
 public class OnBoardingActivity extends AppCompatActivity {
+
+    class OnboardingPageAdapter extends FragmentPagerAdapter {
+
+        ArrayList<Fragment> fragmentList = new ArrayList<>();
+
+        public OnboardingPageAdapter(FragmentManager fragmentManager) {
+            super(fragmentManager);
+        }
+
+        @NonNull
+        @Override
+        public Fragment getItem(int position) {
+            return fragmentList.get(position);
+        }
+
+        @Override
+        public int getCount() {
+            return fragmentList.size();
+        }
+
+        void addFragment(Fragment fragment) {
+            fragmentList.add(fragment);
+        }
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_on_boarding);
+
+        ViewPager viewPager = findViewById(R.id.viewPager);
+        OnboardingPageAdapter pageAdapter = new OnboardingPageAdapter(getSupportFragmentManager());
+        pageAdapter.addFragment(new LoginFragment());
+        pageAdapter.addFragment(new RegisterFragment());
+        viewPager.setAdapter(pageAdapter);
     }
 }
