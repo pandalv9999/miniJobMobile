2999fa5794367ee8862a38986ef063ca71c28da9
Rujun Yao <rujunyao@Rujuns-MacBook-Pro-2.local>
2020-04-14 08:07:20 -0700

    add some layout for drawler

--- app/src/main/java/com/example/minijobmobile/MainActivity.java
+++ app/src/main/java/com/example/minijobmobile/MainActivity.java
@@ -1,14 +1,28 @@
 package com.example.minijobmobile;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+import android.view.View;
+import android.view.Menu;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
+import com.google.android.material.navigation.NavigationView;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.AppBarConfiguration;
+import androidx.navigation.ui.NavigationUI;
+import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 public class MainActivity extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
     }
+
+
 }
